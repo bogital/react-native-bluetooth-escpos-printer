5d325e6272904a79b1ed8f00d59155f204858770
Loïc KAMI <lkami@bogital.com>
2022-10-20 11:50:31 +0100

    Update RNBluetoothManagerModule.java

--- android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/RNBluetoothManagerModule.java
@@ -12,8 +12,6 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-// import android.support.v4.app.ActivityCompat;
-// import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 import com.facebook.react.bridge.*;
